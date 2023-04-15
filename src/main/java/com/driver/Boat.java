ffae67d482293737bdf6d2efe26fb2fe08482ff3
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-04-15 22:11:37 +0000

    Doooooooooneeee

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -11,14 +11,19 @@ public class Boat implements  WaterVehicle {
     private String name;
     private int capacity;
 
-    public void setName(String name) {
+    Boat(String name, int capacity){
         this.name = name;
-    }
-
-    public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
-    
+
+    // public void setName(String name) {
+    //     this.name = name;
+    // }
+
+    // public void setCapacity(int capacity) {
+    //     this.capacity = capacity;
+    // }
+
     public String getVehicleName(){
         return name;
     }
