34a993c7c7a1bcf2e6a4b067dd3275cdefa12246
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-04-15 22:00:45 +0000

    Setter added successfully

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -11,6 +11,14 @@ public class Boat implements  WaterVehicle {
     private String name;
     private int capacity;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+    
     public String getVehicleName(){
         return name;
     }
